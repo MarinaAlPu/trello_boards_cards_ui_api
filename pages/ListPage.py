cb300454bade577cf80511b73ac7b8129d4c7461
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-03-27 19:35:45 +0500

    ui test move card done, workin, clean

--- pages/ListPage.py
+++ pages/ListPage.py
@@ -55,24 +55,8 @@ class ListPage:
 
     @allure.step("Перенести карточку в другую колонку:")
     def move_card(self):
-    # def test_drag_and_drop_onto_element(driver):
-    #     driver.get('https://selenium.dev/selenium/web/mouse_interaction.html')
-
-    #     draggable = driver.find_element(By.ID, "draggable")
-    #     droppable = driver.find_element(By.ID, "droppable")
-    #     ActionChains(driver)\
-    #         .drag_and_drop(draggable, droppable)\
-    #         .perform()
-
-    #     assert driver.find_element(By.ID, "drop-status").text == "dropped"
-
         with allure.step("найти карточку, которую надо перенести в другую колонку"):
             draggable = self.__driver.find_element(By.XPATH, '//span[text()="New card"]')
-            # find_serial_number = browser.find_element_by_xpath('.//span[text()=" + ПЕРЕМЕННАЯ + ")]')
-
-            # find_serial_number = browser.find_element_by_xpath(".//*[contains(text(),'sn-" + serialNumber + "')]")
-
-            # find_serial_number = browser.find_element_by_xpath('.//textarea[text()=" + ПЕРЕМЕННАЯ + ")]')
 
         with allure.step("найти колонку, в которую надо перенести карточку"):
             droppable = self.__driver.find_element(By.XPATH, '//textarea[text()="Second list"]')
